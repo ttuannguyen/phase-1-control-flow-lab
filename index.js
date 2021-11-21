7a6510dde4b6cf5e771c129ead08b92788b32c66
Tuan Nguyen <trentontuannguyen@gmail.com>
2021-11-20 20:34:38 -0500

    commit tree 060cea9a05c6725a728a94e15c2c8a809d78b11a

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 2000 && distance <2500){
+    return 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city == 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let result;
+  switch(tip) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    default:
+      result = 'Bye.';
+      break;
+  }
+  return result;
 }
